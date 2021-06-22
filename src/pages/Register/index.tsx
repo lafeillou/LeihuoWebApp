572a0bc8f5b7af4b232d8e9f2e5eab37e3e70864
lafeillou <lafeillou@126.com>
2021-06-22 11:26:34 +0800

    注册表单填充

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -2,8 +2,49 @@ import React from 'react';
 import './Register.scss';
 import {IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonBackButton, IonTitle, IonContent } from '@ionic/react';
 import  Icon from '../../components/CustomIcon';
+import {useForm, SubmitHandler} from 'react-hook-form';
+import classnames from 'classnames';
+
+type registerForm = {
+    name: string,
+    password: string,
+    phone: string,
+    policeNo: string,
+    sex: string,
+    orgId: number,
+    jurisdiction: string,
+    deptId: number
+}
 
 const Register: React.FC<any> = ({handlePresent}) => {
+
+    const {register, handleSubmit, formState: {errors}} = useForm<registerForm>();
+
+    const onSubmit: SubmitHandler<registerForm> = (data) => {
+        const {name, password, phone, policeNo, sex, orgId, jurisdiction, deptId} = data;
+        console.log(data)
+        // login({
+        //   policeNo: policeCode,
+        //   password: policeCode,
+        //   // name
+        // }).then( res => {
+        //   if (!res.data.success) {
+        //     present({
+        //       message: res.data.msg,
+        //       duration: 3000
+        //     })
+        //   }
+        //   // to do 登录成功后的操作
+        // }).catch(err => {
+        //     present({
+        //       message: err,
+        //       color: 'danger',
+        //       duration: 3000
+        //     })
+        // })
+      }
+
+
     return (
     <IonPage>
         <IonHeader mode="ios">
@@ -15,38 +56,38 @@ const Register: React.FC<any> = ({handlePresent}) => {
             </IonToolbar>
         </IonHeader>
         <IonContent>
-            <div className="registerForm">
+            <form className="registerForm" onSubmit={handleSubmit(onSubmit)}>
                 <div className="input-item-wrap">
                     <Icon type="gerenzhongxin" className="left-icon"/>
-                    <input type="text" className="input-item" placeholder="请输入您的姓名"/>
+                    <input {...register("name", {required: true})} className={classnames('input-item', {error: errors.name})} type="text" placeholder="请输入您的姓名"/>
                 </div>
 
                 <div className="input-item-wrap">
-                    <Icon type="gerenzhongxin" className="left-icon"/>
-                    <input type="number" className="input-item" placeholder="请输入您手机号"/>
+                    <Icon type="shouji-app" className="left-icon"/>
+                    <input type="number" {...register("phone", {required: true})} className={classnames('input-item', {error: errors.phone})} placeholder="请输入您手机号"/>
                 </div>
 
                 <div className="input-item-wrap">
-                    <Icon type="gerenzhongxin" className="left-icon"/>
-                    <input type="text" className="input-item" placeholder="请输入您的警号"/>
+                    <Icon type="caozuo-jinghaoku" className="left-icon"/>
+                    <input type="text" {...register("policeNo", {required: true})} className={classnames('input-item', {error: errors.policeNo})} placeholder="请输入您的警号"/>
                 </div>
 
                 <div className="input-item-wrap">
-                    <Icon type="gerenzhongxin" className="left-icon"/>
-                    <input  className="input-item" placeholder="请选择您的性别"/>
+                    <Icon type="xingbie-app" className="left-icon"/>
+                    <input type="text" {...register("sex", {required: true})} className={classnames('input-item', {error: errors.sex})} placeholder="请选择您的性别"/>
                 </div>
 
                 <div className="input-item-wrap">
-                    <Icon type="gerenzhongxin" className="left-icon"/>
-                    <input  className="input-item" placeholder="请选择您的部门"/>
+                    <Icon type="zuzhi-app" className="left-icon"/>
+                    <input type="text" {...register("jurisdiction", {required: true})} className={classnames('input-item', {error: errors.jurisdiction})} placeholder="请选择您的部门"/>
                 </div>
 
                 <div className="input-item-wrap">
-                    <IonButton expand="block" className="registerBtn">
+                    <IonButton type="submit" expand="block" className="registerBtn">
                     提交注册
                     </IonButton>
                     </div>
-            </div>
+            </form>
         </IonContent>
     </IonPage>
     )

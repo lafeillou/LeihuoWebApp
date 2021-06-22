276861eeded095eff53d9d95a79d005d9a68966e
lafeillou <lafeillou@126.com>
2021-06-22 16:05:13 +0800

    选择部门岗位组件

--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -1,16 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import {IonButton} from '@ionic/react'
+import {IonButton, IonContent} from '@ionic/react'
 import './CustomSelect.scss';
 // import  Icon from './CustomIcon';
 // import classnames from 'classnames';
 
 // import {useForm, SubmitHandler} from 'react-hook-form';
 import {getDepList} from "../api/common";
+import classNames from 'classnames';
 
 const CustomSelectModal: React.FC<{
     onDismiss: () => void;
   }> = ({ onDismiss}) => {
     const [depList, setDepList] = useState([]);
+    // 当前选择的部门
+    const [currentSelectDep, setCurrentSelectDep] = useState<any>(null);
+    // 当前选择的岗位
+    const [currentSelectUnit, setCurrentSelectUnit] = useState<any>(null);
+
+    // 选择部门
+    const handleDepSelect = (o:any) => {
+        setCurrentSelectDep(o);
+    }
+    // 选择岗位（部门里面的小部门）
+    const handleUnitSelect = (o:any) => {
+        setCurrentSelectUnit(o);
+    }
     useEffect(()=>{
         getDepList().then(res => {
             console.log('====getDepList====')
@@ -27,7 +41,32 @@ const CustomSelectModal: React.FC<{
             <input type="text" placeholder="请输入机构名称搜索" />
             <IonButton size="small">搜索</IonButton>
         </form>
-        <div className="depList-wrap"></div>
+        <div className="depList-wrap">
+            <div className="left-part">
+                <IonContent>
+                    {depList.map( o => {
+                        const {orgName, orgId} = o;
+                        return (
+                            <div className={classNames('depItem-wrap', {selected: currentSelectDep?.orgId === orgId})} key={orgId} onClick={() => {handleDepSelect(o)}}>
+                                <span>{orgName}</span>
+                            </div>
+                        )
+                    })}
+                </IonContent>
+            </div>
+            <div className="right-part">
+                <IonContent>
+                {currentSelectDep?.depts.map((o:any) => {
+                    const {deptId, deptName} = o;
+                    return (
+                        <div className={classNames('unitItem-wrap', {selected: currentSelectUnit?.deptId === deptId } )} key={deptId} onClick={() => {handleUnitSelect(o)}}>
+                            <span>{deptName}</span>
+                        </div>
+                    )
+                })}
+                </IonContent>
+            </div>
+        </div>
     </div>
   </div>)
     
